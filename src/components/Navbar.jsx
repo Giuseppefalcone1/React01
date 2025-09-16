3b6f0540b8868c0d2fd78c38fe0cee71b599077b
giuseppefalcone1 <giuseppe.falcone1@hotmail.com>
2025-09-16 16:05:30 +0200

    aggiunti pulsanti incrementa e aggiungi con useState

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from "react";
 function Navbar(){
     
+
+    const[count, update] = useState(0);
+    function incrementa(){
+        update(count+1);
+    }
+
+    const newItem = "nuovo elemento aggiunto";
+    const[array, setArray] = useState([1,2,3])
+    const addItem = ()=>{
+        setArray([...array,newItem])
+        console.log(array)
+    }
+
     return(
     <>
+        <p>valore - {count}</p>
+        <button onClick={incrementa}>incrementa</button>
+        <button onClick={addItem}>aggiungi</button>
        <nav style={{ borderRadius: "5px", padding: "10px", backgroundColor: "#494949ff" }}>
            <ul style={{ display: "flex", gap: "5px", listStyle: "none", margin: 0, padding: 0, color: "white" }}>
            <button>Home</button>
