a9f2f8ba057e8fa1e9192e0ba79534bd37d4ec6a
giuseppefalcone1 <giuseppe.falcone1@hotmail.com>
2025-10-08 17:51:35 +0200

    aggiornato form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ function Form(){
     colore:"",
   })
 
-  const handleChange = (e) => {
+    const handleChange = (e) => {
 
     const name = e.target.name;
     const value = e.target.value;  // 1. catturiamo il name e value dell'input
@@ -16,16 +16,34 @@ function Form(){
     console.log(name, value);
   }
 
+    //handleSubmit
+    //handleClick
 
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
 
-  return(
-    <>
-      <input type="text"
-        name='produttore'
-        value={formData.produttore} //catturiamo l'input formData.produttore e lo inscatoliamo nel value
-        onChange={handleChange} //ogni cambiamento richiamiamo la funzione handleChange
-      />
-    </>
+            <input type="text"
+                name='produttore'
+                value={formData.produttore} //catturiamo l'input formData.produttore e lo inscatoliamo nel value
+                onChange={handleChange} //ogni cambiamento richiamiamo la funzione handleChange
+            />
+
+            <input type="text"
+                name='modello'
+                value={formData.modello}
+                onChange={handleChange}
+            />
+
+            <input type="text"
+                name='anno'
+                value={formData.anno}
+                onChange={handleChange}
+            />
+
+                <button onClick={handleClick}></button>
+            </form>
+        </>
   )
 
 }
